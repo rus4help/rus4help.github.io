404b6970a1138623e880f9ac4c348df72417188c
Денис Нихаев <denis.nikhaev@gmail.com>
2019-08-06 22:17:28 +0300

    Изменение структуры проектов

---
+++ projects/layout designer/progect-grid_003/js/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(`.hamburger`);
+
+hamburger.addEventListener(`click`, (e) => {
+  e.target.closest(`.hamburger`).classList.toggle(`hamburger--active`);
+  e.target.closest(`.menu`).querySelector(`.menu__list`).classList.toggle(`menu__list--active`);
+});
