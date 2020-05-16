38c8898ca6fd0e1afb31318d2f4634bfff730453
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-05-16 22:33:53 +0300

    Создал метод для наполнения массива $routes

--- projects/programmer/php/frameworks/fw-wfms/vendor/core/Router.php
+++ projects/programmer/php/frameworks/fw-wfms/vendor/core/Router.php
@@ -6,4 +6,9 @@ class Router
     protected static $routes = [];
     protected static $route = [];
 
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
 }
