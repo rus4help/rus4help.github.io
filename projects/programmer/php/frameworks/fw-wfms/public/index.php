c6c6122963486649c1ef35d92b414d8f8a0cd4fe
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-05-16 23:15:26 +0300

    Проверяю работоспособность метода

--- projects/programmer/php/frameworks/fw-wfms/public/index.php
+++ projects/programmer/php/frameworks/fw-wfms/public/index.php
@@ -10,3 +10,9 @@ Router::add('posts/', ['controller' => 'Posts', 'action' => 'index']);
 Router::add('', ['controller' => 'Main', 'action' => 'index']);
 
 debug(Router::getRoutes());
+
+if(Router::matchRoute($query)) {
+    debug(Router::getRoute());
+}else {
+    echo '404';
+}
